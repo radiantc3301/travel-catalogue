8c0048ad0fb628d9ec2b208ca9eef68198954371
Anand Sasikumar <115141207+radiantc3301@users.noreply.github.com>
2023-05-07 12:36:20 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        id: 1,
+        title: "Mount Fuji",
+        location: "J A P A N",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://cdn.britannica.com/03/75503-050-F65891FA/volcanic-cone-Japan-Mount-Fuji.jpg"
+    },
+    {   
+        id: 2,
+        title: 'Sydney Opera House',
+        location: 'A U S T R A L I A',
+        googleMapsUrl: 'https://www.google.com/maps/search/?q=Sydney+Opera+House+place_id:ChIJ3S-JXmauEmsRUcIaWtf4MzE',
+        startDate: '27 May, 2021',
+        endDate: '8 June, 2021',
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        imageUrl: 'https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/A-iStock-49888866_hwah23.jpg'
+        
+    },
+    {   
+        id: 3,
+        title: 'Geirangerfjord',
+        location: 'N O R W A Y',
+        googleMapsUrl: 'https://maps.google.com/maps?q=geirangerfjord+google+maps&rlz=1C1CHZN_enIN1028IN1028&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiBkcGq85n8AhUK1jgGHTYvBAwQ_AUoAXoECAEQAw',
+        startDate: '1 Oct, 2021',
+        endFate: '18 Nov, 2021',
+        description: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Geirangerfjord_.jpg/1200px-Geirangerfjord_.jpg'
+        
+    }
+]
