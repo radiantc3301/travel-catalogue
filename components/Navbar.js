603ffdd9210cc5a1989aab9501093a6ac541537c
Anand Sasikumar <115141207+radiantc3301@users.noreply.github.com>
2023-05-07 12:41:46 +0530

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Navbar() {
+    return(
+        <div id = 'navbar'>
+            <h3>my travel journal</h3>
+        </div>
+    )
+}
